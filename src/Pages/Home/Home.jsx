af6c7316e7aff4f46473d21eef52198cdc59c23a
NickN1994 <nickneggers@hotmail.com>
2024-02-17 10:24:09 +0100

    css update

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -13,7 +13,6 @@ function Home() {
     const [activityList, setActivityList] = useState([]);
 
 
-
     useEffect(() => {
         const abortController = new AbortController();
         const token = localStorage.getItem('token');
@@ -68,7 +67,7 @@ function Home() {
     }, []);
 
     return (
-
+        <>
             <div className='outer-container'>
                 <div className='inner-container'>
                     {isLoading && (
@@ -76,37 +75,64 @@ function Home() {
                         </div>
                     )}
 
+                    <div className="colums">
 
-                        <h1>Welkom {userData.name}</h1>
-                        <h2>Je staat ingeschreven voor deze activiteiten</h2>
-                        <div className="parent-activity-box">
+                        <section className="colum-one">
+                            <h1>Welkom {userData.name}</h1>
+                            <p>Leuk dat je hier bent. In deze omgeving vind je alles terug wat je nodig hebt voor je
+                                persoonlijke reis bij Het Land van Licht.
+                                Ga naar Activiteiten om jezelf in te schrijven voor een activiteit. Kan je na
+                                inschrijving toch niet? Dan kun je met één druk op knop jezelf weer uitschrijven.
+                                Om te zien voor elke activiteiten jij ingeschreven staat, vind je onderaan deze
+                                pagina.</p>
+
+                            <p>Ga naar de pagina leeromgeving om de meditaties van Het land van Licht te ontdekken die
+                                je onder andere op de locatie van het Land van Licht kan doen.
+                                Daarnaast vind je hier ook de meditaties en oefeningen die horen bij het Spel des
+                                Levens.</p>
+
+                            <p>Mocht je vragen hebben, dan kan je via het contact formulier contact opnemen. Is het
+                                dringend? Dan zijn we ook via Whatsapp te bereiken.</p>
+
+                            <p>Fijne reis, Nick & Kirstie.</p>
+
+                        </section>
+
+                        <section className="colum-two">
+                            <figure className='colum-two'>
+                                <img src={fotoHome} alt="Afbeelding Nick en Kirstie"/>
+                            </figure>
+                        </section>
+                    </div>
+                </div>
+            </div>
+            <div className="outer-container">
+                <div className="inner-container">
+                    <h2>Je staat ingeschreven voor deze activiteiten</h2>
+                    <div className="parent-activity-box">
                         {activityList ?
-                        activityList.map((activity) => (
-                            <ActivityBox
-                                key={activity.id}
-                                id={activity.id}
-                                name={activity.name}
-                                participants={activity.participants}
-                                teacher={activity.teacher}
-                                date={activity.date}
-                                time={activity.time}
-                                activityInfo={activity.activityInfo}
-                            />
-                        ))
+                            activityList.map((activity) => (
+                                <ActivityBox
+                                    key={activity.id}
+                                    id={activity.id}
+                                    name={activity.name}
+                                    participants={activity.participants}
+                                    teacher={activity.teacher}
+                                    date={activity.date}
+                                    time={activity.time}
+                                    activityInfo={activity.activityInfo}
+                                />
+                            ))
                             :
                             <div>
-                            <p>Je bent nergens voor ingeschreven.</p>
+                                <p>Je bent nergens voor ingeschreven.</p>
                                 <Link to={"/activiteiten"} className="btn btn-orange">Klik hier voor activiteiten</Link>
                             </div>
                         }
-                        </div>
-
-
-                    {/*<figure className='colum-two'>*/}
-                    {/*    <img src={fotoHome} alt="Afbeelding Nick en Kirstie"/>*/}
-                    {/*</figure>*/}
+                    </div>
                 </div>
             </div>
+        </>
 
 
     )
